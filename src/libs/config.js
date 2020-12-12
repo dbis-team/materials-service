244d1e9e8fe85ab5c5023ca83fec07651d7ba149
Andriy Antonenko <andriyantonenko3.16@gmail.com>
2020-12-12 19:35:29 +0200

    Add port and host

--- src/libs/config.js
+++ src/libs/config.js
@@ -1,6 +1,8 @@
 require('dotenv').config()
 
 module.exports = {
+  PORT: +process.env.PORT,
+  HOST: process.env.HOST,
   GOOGLE_CLOUD_PROJECT_ID: process.env.GOOGLE_CLOUD_PROJECT_ID,
   MATERIALS_BUCKET_NAME: process.env.MATERIALS_BUCKET_NAME,
   GOOGLE_CLOUD_SECRET_FILE_NAME: process.env.GOOGLE_CLOUD_SECRET_FILE_NAME,
--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
 const { configureFastifyServer } = require('./server_setup');
 const { createConnection } = require('./libs/mongo');
+const { HOST, PORT } = require('./libs/config');
 
 (async () => {
   const server = configureFastifyServer();
 
   try {
     await createConnection();
-    await server.listen(3000);
+    await server.listen(PORT, HOST, () => {
+      console.info(`Server running on host ${HOST} and port ${PORT}`);
+    });
   } catch (err) {
     server.log.error(err);
     process.exit(1);
